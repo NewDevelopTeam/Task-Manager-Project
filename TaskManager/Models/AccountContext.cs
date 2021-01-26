6784a91b73b3dd94c7f150666c99745b5c947f27
kondratio <konhote@gmail.com>
2021-01-26 22:49:27 +0300

    added database context that coordinates EF functionality for a given data model

---
+++ TaskManager/Models/AccountContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Models
+{
+    public class AccountContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public AccountContext(DbContextOptions<AccountContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
