107a60d6b1ce5180944c614e063132cbd6692fe6
kondratio <konhote@gmail.com>
2021-01-30 20:13:30 +0300

    added a viewmodel for registration

---
+++ TaskManager/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace TaskManager.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Не указан Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Подтверждение пароля")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Введённые пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
