1c34cc82b8ab7bcb41f5d1001506779cf046fbb3
kondratio <konhote@gmail.com>
2021-02-13 15:35:11 +0300

    added attributes needed for data validation in the client-side and a server-side as well for register form

--- TaskManager/ViewModels/RegisterViewModel.cs
+++ TaskManager/ViewModels/RegisterViewModel.cs
@@ -7,16 +7,19 @@ namespace TaskManager.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Не указан Email")]
-        [EmailAddress]
+        [Required(ErrorMessage = "Не указана почта")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Некорректно указана почта")]
         public string Email { get; set; }
-
+        
         [Required(ErrorMessage = "Не указан пароль")]
+        [RegularExpression(@"^[\w]*$", ErrorMessage = "Допускаются только латинские буквы и цифры")]
+        [StringLength(50, ErrorMessage = "Пароль должен содержать не менее {2} символов", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
-        [Display(Name = "Подтверждение пароля")]
+        [Required(ErrorMessage = "Повторно укажите пароль")]
+        [RegularExpression(@"^[\w]*$", ErrorMessage = "Допускаются только латинские буквы и цифры")]
+        [StringLength(18, ErrorMessage = "Пароль должен содержать не менее {2} символов", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Введённые пароли не совпадают")]
         public string ConfirmPassword { get; set; }
