45a4bf53455498614542739ca5e647f623d6d2b9
kondratio <konhote@gmail.com>
2021-02-13 16:16:02 +0300

    updated a page controller to make access to basic pages only for authrozed users

--- TaskManager/Controllers/PagesController.cs
+++ TaskManager/Controllers/PagesController.cs
@@ -3,19 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TaskManager.Controllers
-{
+{ 
     public class PagesController : Controller
     {
+        [Authorize]
         public IActionResult Cards()
         {
             return View();
         }
+        [Authorize]
         public IActionResult Boards()
         {
             return View();
         }
+        [Authorize]
         public IActionResult Notifications()
         {
             return View();
