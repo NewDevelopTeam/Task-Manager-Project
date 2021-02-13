a632b4559e10942313d607b8cb7e599f9befdc7a
kondratio <konhote@gmail.com>
2021-02-13 15:34:30 +0300

    added attributes needed for data validation in the client-side and a server-side as well for Login user

--- TaskManager/ViewModels/LoginViewModel.cs
+++ TaskManager/ViewModels/LoginViewModel.cs
@@ -8,10 +8,13 @@ namespace TaskManager.ViewModels
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Не указан Email")]
+        [Required(ErrorMessage = "Не указана почта")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Некорректно указана почта")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Не указан пароль")]
+        [RegularExpression(@"^[\w]*$", ErrorMessage = "Допускаются только латинские буквы и цифры")]
+        [StringLength(50, ErrorMessage = "Пароль должен содержать не менее {2} символов", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
