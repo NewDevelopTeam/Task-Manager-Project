be7e486cd2a74d4b3d50e79170015207cf2808f3
kondratio <konhote@gmail.com>
2021-03-31 21:58:59 +0300

    Add models for personal and multiple dashboards

---
+++ TaskManager/Models/BoardsPageModels/MultiDashboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models.BoardsPageModels
+{
+    public class MultiDashboard
+    {
+        public int Id { get; set; }
+        public string DashboardName { get; set; }
+        public int PositionNo { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
