3e75be7fb78d5609e89dd845137fdfe0b67b7b30
kondratio <konhote@gmail.com>
2021-03-31 22:02:50 +0300

    Add viewmodels for personal and multiple dashboards

---
+++ TaskManager/ViewModels/BoardsPageViewModels/BoardsPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Models.BoardsPageModels;
+
+namespace TaskManager.ViewModels.BoardsPageViewModels
+{
+    public class BoardsPageViewModel
+    {
+        [MaxLength(20, ErrorMessage = "Название доски не более 20 символов")]
+        [Required(ErrorMessage = "Укажите название персональной доски")]
+        public string PersonalBoardName { get; set; }
+
+        [MaxLength(20,ErrorMessage ="Название доски не более 20 символов")]
+        [Required(ErrorMessage = "Укажите название многопользовательской доски")]
+        public string MultiBoardName { get; set; }
+        public List<PersonalDashboard> PersonalDashboards { get; set; }
+        public List<MultiDashboard> MultiDashboards { get; set; }
+    }
+}
