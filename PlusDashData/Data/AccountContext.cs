d31ff97ff9f3ec4bd491cb369b5eb3b126e2f3c8
kondratio <konhote@gmail.com>
2021-04-02 21:49:08 +0300

    Refactoring: Transfer a database context and corresponding models to a particular class library

--- PlusDashData/Data/AccountContext.cs
+++ PlusDashData/Data/AccountContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Models.BoardsPageModels;
 
 namespace TaskManager.Models
 {
@@ -10,9 +11,11 @@ namespace TaskManager.Models
     {
         public DbSet<User> Users { get; set; }
         public DbSet<PersonalCard> PerCards {get; set;}
+        public DbSet<PersonalDashboard> PerDashBoards { get; set; }
+        public DbSet<MultiDashboard> MultiDashBoards { get; set; }
         public AccountContext(DbContextOptions<AccountContext> options):base(options)
         {
-            
+            Database.EnsureCreated();
         }
 
     }
