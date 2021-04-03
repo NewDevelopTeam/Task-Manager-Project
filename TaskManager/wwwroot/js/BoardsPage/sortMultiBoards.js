2d7986243fce2b324e7753eacbcd82e49c60c5ff
kondratio <konhote@gmail.com>
2021-04-03 17:30:14 +0300

    Add js scripts for sorting personal and multiple boards

---
+++ TaskManager/wwwroot/js/BoardsPage/sortMultiBoards.js
@@ -0,0 +1,38 @@
+﻿import Sortable from '/vendor/sortablejs/modular/sortable.complete.esm.js';
+
+$(document).ready(function () {
+	const boardsContainer = document.querySelector('.allMultiBoards');
+
+	var sortable = new Sortable(boardsContainer, {
+		animation: 350,
+		chosenClass: 'sortable-chosen',
+		dragClass: 'sortable-drag',
+		ghostClass: "sortable-ghost",
+		forceFallback: true,
+		fallbackClass: "dragged-item",
+		dataIdAttr: 'data-id',
+		filter: '.isNotDraggable',
+		onSort: function () {
+			var boardsPositions = sortable.toArray();
+			SaveMultiBoardsPositions(boardsPositions);
+		},
+		onStart: function () {
+			$('html').addClass("draggable-cursor");
+		},
+		onEnd: function () {
+			$('html').removeClass("draggable-cursor");
+		}
+	});
+});
+
+function SaveMultiBoardsPositions(positions) {
+	$.ajax({
+		method: 'GET',
+		url: 'https://localhost:44363/boards/updatemultiboards',
+		contentType: 'application/json',
+		data: $.param({ data: positions }, true),
+		error: function () {
+			alert("Произошел сбой");
+		}
+	});
+}
---
+++ TaskManager/wwwroot/js/BoardsPage/sortPersonalBoards.js
@@ -0,0 +1,39 @@
+﻿import Sortable from '/vendor/sortablejs/modular/sortable.complete.esm.js';
+
+$(document).ready(function () {
+    const boardsContainer = document.querySelector('.allPersonalBoards');
+
+	var sortable = new Sortable(boardsContainer, {
+		animation: 350,
+		chosenClass: 'sortable-chosen',
+		dragClass: 'sortable-drag',
+		ghostClass: "sortable-ghost",
+		forceFallback: true,
+		fallbackClass: "dragged-item",
+		dataIdAttr: 'data-id',
+		filter: '.isNotDraggable',
+		onSort: function () {
+			var boardsPositions = sortable.toArray();
+			SavePersonalBoardsPositions(boardsPositions)
+		},
+		onStart: function () {
+			$('html').addClass("draggable-cursor");
+		},
+		onEnd: function () {
+			$('html').removeClass("draggable-cursor");
+		}
+	});
+});
+
+function SavePersonalBoardsPositions(positions) {
+	$.ajax({
+		method: 'GET',
+		url: 'https://localhost:44363/boards/updatepersonalboards',
+		contentType: 'application/json',
+		data: $.param({ data: positions }, true),
+		error: function () {
+			alert("Произошел сбой");
+        }
+	});
+}
+
