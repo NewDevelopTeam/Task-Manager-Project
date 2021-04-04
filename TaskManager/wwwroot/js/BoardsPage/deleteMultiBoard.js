db66bd4586f0ba0ccaead4f071ec9694b732a4ec
kondratio <konhote@gmail.com>
2021-04-04 20:16:01 +0300

    Add js scripts for removing personal and multiple boards

---
+++ TaskManager/wwwroot/js/BoardsPage/deleteMultiBoard.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+	$('.multiBoard-delete').click(function () {
+		let idValue = event.target.closest('.Board-container').dataset.id;
+		$('.delete-multiBoard__button').attr('id', idValue);
+	});
+	$('.delete-multiBoard__button').click(function () {
+		let id = $('.delete-multiBoard__button').attr('id');
+		DeleteBoard(id);
+	});
+})
+function DeleteBoard(idBoard) {
+	$.ajax({
+		method: 'GET',
+		url: 'https://localhost:44363/boards/deletemultiboard',
+		contentType: 'application/json',
+		data: $.param({ id: idBoard }, true),
+		success: function () {
+			window.location.href = "/pages/boards";
+		},
+		error: function () {
+			alert("Произошел сбой");
+		}
+	});
+}
