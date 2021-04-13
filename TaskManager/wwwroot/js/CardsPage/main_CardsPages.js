5b2df9a264a924f7e2aa285e5d677ea1c4dcc6fd
kondratio <konhote@gmail.com>
2021-04-13 05:00:20 +0300

    Add a js script and css file for preloader to Layout.cshtml

--- TaskManager/wwwroot/js/CardsPage/main_CardsPages.js
+++ TaskManager/wwwroot/js/CardsPage/main_CardsPages.js
@@ -1,15 +1,21 @@
 ﻿
 
+import '../preloader.js';
 import { ShowPersonalCards } from './showCards.js';
 import './deleteCards.js';
 import './editCards.js';
 import './sortCards.js';
 
-function ready(func) {
-	if (document.readyState != 'loading') {
-		func();		
-	} else {
-		document.addEventListener('DOMContentLoaded', func);	
-	}
-}
-ready(ShowPersonalCards);
+
+ShowPersonalCards();
+
+//function ready() {
+//	if (document.readyState != 'loading') {
+//		console.log("вызов showpersonalcards")
+//		ShowPersonalCards();		
+//	} else {
+//		console.log("отложен вызов");
+//		document.addEventListener('DOMContentLoaded', ShowPersonalCards);	
+//	}
+//}
+//ready();
