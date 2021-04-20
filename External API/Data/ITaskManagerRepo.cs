3de70b6665971762a944aa84c222474a6493cd03
kondratio <konhote@gmail.com>
2021-04-20 19:54:55 +0300

    Add empty external web api

---
+++ External API/Data/ITaskManagerRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlusDashData.Data;
+
+namespace External_API.Data
+{
+    public interface ITaskManagerRepo
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+    }
+}
